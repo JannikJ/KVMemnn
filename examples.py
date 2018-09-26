6e0fa4e26f32b5c3b219ab90316aab1d122c48e9
Jannik <jannik@outlook.de>
2018-09-26 12:23:32 +0200

    bug fixes for new examples.py, easier to adapt to different test data, fixed indents

--- examples.py
+++ examples.py
@@ -5,24 +5,26 @@ import argparse
 import torch
 import torch.nn.functional as F
 import numpy as np
-from reader import Data,Vocabulary
+from reader import Data, Vocabulary
 from model.memnn import KVMMModel
 from math import log
 from numpy import array
 from numpy import argmax
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 # beam search
-def run_example(model, kbs,vocabulary, text, groundtruth):
+def run_example(model, kbs, vocabulary, text, groundtruth):
     encoded = vocabulary.string_to_int(text)
-    input_tensors = torch.from_numpy(np.expand_dims(encoded,axis=0))
-    kbs = torch.from_numpy(np.expand_dims(kbs,axis=0))
+    input_tensors = torch.from_numpy(np.expand_dims(encoded, axis=0))
+    kbs = torch.from_numpy(np.expand_dims(kbs, axis=0))
     prediction = model(input_tensors, kbs[0])
-    prediction = F.softmax(prediction, dim=2) 
-    #print(prediction[0])
-    #sys.exit(1)
-    print('input:',text)
-    print('groundtruth:',groundtruth)
+    prediction = F.softmax(prediction, dim=2)
+    # print(prediction[0])
+    # sys.exit(1)
+    print('input:', text)
+    print('groundtruth:', groundtruth)
     print('symbol prediction:', ' '.join(vocabulary.int_to_string(prediction[0].max(1)[1].detach().cpu().numpy())))
     output = ' '.join(vocabulary.int_to_string(prediction[0].max(1)[1].detach().cpu().numpy()))
     return output
@@ -31,7 +33,7 @@ def run_example(model, kbs,vocabulary, text, groundtruth):
 def run_examples(model, kbs, vocabulary, examples, groundtruths):
     predicted = []
     input = []
-    for example,groundtruth in zip(examples,groundtruths):
+    for example, groundtruth in zip(examples, groundtruths):
         print('~~~~~')
         input.append(example)
         predicted.append(run_example(model, kbs, vocabulary, example, groundtruth))
@@ -40,21 +42,22 @@ def run_examples(model, kbs, vocabulary, examples, groundtruths):
 
 if __name__ == "__main__":
     pad_length = 20
-    df = pd.read_csv("data/test_data.csv")
+    dialog_type = " - weather"
+    df = pd.read_csv("data/test_data" + dialog_type + ".csv", encoding="ISO-8859-1", delimiter=';')
     inputs = list(df["inputs"])
     outputs = list(df["outputs"])
     vocab = Vocabulary('data/vocabulary.json', padding=pad_length)
 
-    kb_vocabulary = Vocabulary('data/vocabulary.json',padding = 4)
+    kb_vocabulary = Vocabulary('data/vocabulary.json', padding=4)
 
     model = KVMMModel(pad_length=20,
-                  embedding_size=200,
-                  batch_size=1,
-                  vocab_size=vocab.size(),
-                  n_chars=vocab.size(),
-                  n_labels=vocab.size(),
-                  encoder_units=200,
-                  decoder_units=200).to(device)
+                      embedding_size=200,
+                      batch_size=1,
+                      vocab_size=vocab.size(),
+                      n_chars=vocab.size(),
+                      n_labels=vocab.size(),
+                      encoder_units=200,
+                      decoder_units=200).to(device)
     weights_file = "model_weights.pytorch"
     model.load_state_dict(torch.load(weights_file))
 
@@ -64,14 +67,13 @@ if __name__ == "__main__":
     # print(kbs[:3])
     kbs = np.array(list(map(kb_vocabulary.string_to_int, kbs)))
     kbs = np.repeat(kbs[np.newaxis, :, :], 1, axis=0)
-    data = run_examples(model, kbs,vocab, inputs, outputs)
-    df=pd.DataFrame(columns=["inputs","outputs","prediction"])
-    d = {'outputs':[],'inputs':[],'predictions': []}
+    data = run_examples(model, kbs, vocab, inputs, outputs)
+    df = pd.DataFrame(columns=["inputs", "outputs", "prediction"])
+    d = {'outputs': [], 'inputs': [], 'predictions': []}
     for i, o, p in zip(inputs, outputs, data):
         d["outputs"].append(str(o))
         d["inputs"].append(str(i))
         d["predictions"].append(str(p))
     df = pd.DataFrame(d)
-    df.to_csv("output_kb.csv")
+    df.to_csv("output_kb.csv", encoding="ISO-8859-1", sep=";")
     # print(outputs)
-
