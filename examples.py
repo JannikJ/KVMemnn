6b65717d346e0da91fa761c7d704e340f100130c
Jannik <jannik@outlook.de>
2018-10-23 12:58:48 +0200

    some adaptions

--- examples.py
+++ examples.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 import pandas as pd
 import torch
@@ -12,6 +14,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # beam search
 def run_example(model, kbs, vocabulary, text, groundtruth):
     encoded = vocabulary.string_to_int(text)
+    if encoded.__contains__(1518):
+        print("UNK")
+    for enc in encoded:
+        if enc > 6085:
+            print("BIG")
     input_tensors = torch.from_numpy(np.expand_dims(encoded, axis=0))
     kbs = torch.from_numpy(np.expand_dims(kbs, axis=0))
     prediction = model(input_tensors, kbs[0])
@@ -19,8 +26,12 @@ def run_example(model, kbs, vocabulary, text, groundtruth):
     # print(prediction[0])
     # sys.exit(1)
     print('input:', text)
+    print("encoded input:", vocabulary.int_to_string(encoded))
     print('groundtruth:', groundtruth)
     print('symbol prediction:', ' '.join(vocabulary.int_to_string(prediction[0].max(1)[1].detach().cpu().numpy())))
+    for out in prediction[0].max(1)[1].detach().cpu().numpy():
+        if out > 6085:
+            print("BIG")
     output = ' '.join(vocabulary.int_to_string(prediction[0].max(1)[1].detach().cpu().numpy()))
     return output
 
@@ -36,16 +47,27 @@ def run_examples(model, kbs, vocabulary, examples, groundtruths):
 
 
 if __name__ == "__main__":
+    use_original = True
     pad_length = 20
-    dialog_type = " - navigate"
+    dialog_type = " - original"
     underscore = "_"
     kb = " - kb"
-    df = pd.read_csv("data/test_data" + underscore + dialog_type + kb + ".csv", encoding="ISO-8859-1", delimiter=',')
-    inputs = list(df["input"])
-    outputs = list(df["output"])
-    vocab = Vocabulary('data/vocabulary' + dialog_type + '.json', padding=pad_length)
+    if use_original:
+        old_dialog_type = dialog_type
+        dialog_type = ""
+        underscore = ""
+        kb = ""
+    df = pd.read_csv("data/val_data" + underscore + dialog_type + kb + ".csv", encoding="ISO-8859-1", delimiter=',')
+    if use_original:
+        dialog_type = old_dialog_type
+        inputs = list(df["inputs"])
+        outputs = list(df["outputs"])
+    else:
+        inputs = list(df["input"])
+        outputs = list(df["output"])
+    vocab = Vocabulary('data/vocabulary-train' + dialog_type + '.json', padding=pad_length)
 
-    kb_vocabulary = Vocabulary('data/vocabulary' + dialog_type + '.json', padding=4)
+    kb_vocabulary = Vocabulary('data/vocabulary-train' + dialog_type + '.json', padding=4)
 
     model = KVMMModel(pad_length=20,
                       embedding_size=200,
@@ -55,14 +77,16 @@ if __name__ == "__main__":
                       n_labels=vocab.size(),
                       encoder_units=200,
                       decoder_units=200).to(device)
-    weights_file = "model_weights_navigate_iter_500000.pytorch"
-    model.load_state_dict(torch.load(weights_file))
+    weights_file = "weights/model_weights_original_iter_500000.pytorch"
+    model.load_state_dict(torch.load(weights_file, map_location='cpu'))
 
     kbfile = "data/normalised_kbtuples.csv"
     df = pd.read_csv(kbfile)
-    kbs = list(df["subject"] + " " + df["relation"])
+    kbs = list(df["subject"] + "_" + df["relation"])
+    kbs = [re.sub(" ", "_", kb) for kb in kbs]
     # print(kbs[:3])
     kbs = np.array(list(map(kb_vocabulary.string_to_int, kbs)))
+    # TODO: Fix kb here
     kbs = np.repeat(kbs[np.newaxis, :, :], 1, axis=0)
     data = run_examples(model, kbs, vocab, inputs, outputs)
     df = pd.DataFrame(columns=["inputs", "outputs", "prediction"])
