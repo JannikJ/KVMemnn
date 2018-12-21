54c36a7f7ea027972b94bab3888644c26c8a6d42
Jannik <jannik@outlook.de>
2018-12-21 14:39:07 +0100

    adaptions to examples module

--- examples.py
+++ examples.py
@@ -36,20 +36,22 @@ def run_examples(model, kbs, vocabulary, examples, groundtruths):
     return predicted
 
 
-def main_examples(dialog_type, underscore, kb, iteration=500000, delimiter=","):
+def main_examples(dialog_type, underscore, kb, iteration=500000, delimiter=",", final_folder="", test_mode=""):
+    if final_folder == "":
+        final_folder = dialog_type[3:]
     try:
-        df = pd.read_csv("data/test_data" + underscore + dialog_type + kb + ".csv", encoding="ISO-8859-1", delimiter=delimiter)
+        df = pd.read_csv("data/test_data" + underscore + test_mode + dialog_type + kb + ".csv", encoding="ISO-8859-1", delimiter=delimiter)
     except FileNotFoundError:
-        df = pd.read_csv("../data/test_data" + underscore + dialog_type + kb + ".csv", encoding="ISO-8859-1", delimiter=delimiter)
+        df = pd.read_csv("../data/test_data" + underscore + test_mode + dialog_type + kb + ".csv", encoding="ISO-8859-1", delimiter=delimiter)
     inputs = list(df["input"])
     outputs = list(df["output"])
     actual_clusters = list(df["actual_cluster"])
     try:
-        vocab = Vocabulary('data/vocabulary-train' + dialog_type + '.json', padding=pad_length)
-        kb_vocabulary = Vocabulary('data/vocabulary-train' + dialog_type + '.json', padding=4)
+        vocab = Vocabulary('data/vocabulary-train' + dialog_type + ' - perfect decomposition - preprocessedFinal.json', padding=pad_length)
+        kb_vocabulary = Vocabulary('data/vocabulary-train' + dialog_type + ' - perfect decomposition - preprocessedFinal.json', padding=4)
     except FileNotFoundError:
-        vocab = Vocabulary('../data/vocabulary-train' + dialog_type + '.json', padding=pad_length)
-        kb_vocabulary = Vocabulary('../data/vocabulary-train' + dialog_type + '.json', padding=4)
+        vocab = Vocabulary('../data/vocabulary-train' + dialog_type + ' - perfect decomposition - preprocessedFinal.json', padding=pad_length)
+        kb_vocabulary = Vocabulary('../data/vocabulary-train' + dialog_type + ' - perfect decomposition - preprocessedFinal.json', padding=4)
 
     model = KVMMModel(pad_length=20,
                       embedding_size=200,
@@ -59,7 +61,7 @@ def main_examples(dialog_type, underscore, kb, iteration=500000, delimiter=","):
                       n_labels=vocab.size(),
                       encoder_units=200,
                       decoder_units=200).to(device)
-    weights_file = "weights/model_weights_" + dialog_type[3:] + "_iter_" + str(iteration) + ".pytorch"
+    weights_file = "final-" + final_folder + "/model_weights_" + dialog_type[3:] + "_iter_" + str(iteration) + ".pytorch"
     try:
         try:
             model.load_state_dict(torch.load(weights_file))
@@ -94,8 +96,8 @@ def main_examples(dialog_type, underscore, kb, iteration=500000, delimiter=","):
 
 
 if __name__ == "__main__":
-    dialog_type = " - navigate"
+    dialog_type = " - full"
     underscore = "_"
     kb = " - kb"
     result_df = main_examples(dialog_type, underscore, kb)
-    result_df.to_csv("output_kb" + dialog_type + ".csv", encoding="ISO-8859-1", sep=";")
+    result_df.to_csv("final-full/output_kb" + dialog_type + ".csv", encoding="ISO-8859-1", sep=";")
